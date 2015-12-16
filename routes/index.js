fa1f0088f698825470490cf4a02528b227c67801
Uehara25 <akiya.noface@gmail.com>
2015-12-16 13:58:27 +0900

    change action to post request to "/"
    change to display "showname.jade"

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next){
-  res.json(req.body)
+  res.render('showname', {name: req.body["name"]})
 });
 
 module.exports = router;
